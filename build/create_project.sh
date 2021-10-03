9e2b3b93226ce7919fea10a43eb226cc683b240d
= <=>
2021-10-03 18:34:30 +0200

    init

--- build/create_project.sh
+++ build/create_project.sh
@@ -5,10 +5,6 @@ set -eo pipefail
 BUILD_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT_DIR="$(dirname "${BUILD_DIR}")";
 
-# create directories
-#mkdir -p  "${PROJECT_DIR}"/var/log var/cache
-#chmod 777 "${PROJECT_DIR}"/var/log var/cache
-
 IMAGE="composer"
 TAG="2.1.8"
 
